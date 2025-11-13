257cdc469b619245803e642dbd92f8495171219b
carol-puentes <122763536+carol-puentes@users.noreply.github.com>
2025-11-13 16:22:04 -0500

    arreglo diseño

--- src/App.tsx
+++ src/App.tsx
@@ -119,19 +119,8 @@ export default function App() {
 
             <h1 className="text-main">!UNETE A LA DIVERSION! 🎲</h1>
 
-            {/* <label>ID de sala:</label> */}
-            {/* <br /> */}
-
-            {/* <input
-              className="input"
-              value={roomId}
-              onChange={(e) => setRoomId(e.target.value)}
-            /> */}
-
-            {/* <InputCardComponent/> */}
-
             <div className="wave-group">
-              <div  className="input-container">
+              <div className="input-container">
                 <input
                   required
                   type="text"
@@ -148,18 +137,47 @@ export default function App() {
 
             <br />
 
-            <label>Tu nombre:</label>
-            <input
-              className="input"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
+            <div className="wave-group">
+              <div className="input-container">
+                <input
+                  required
+                  type="text"
+                  className="input"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+                <span className="bar" />
+                <label className="label">
+                  <span className="label-char">Nombre</span>
+                </label>
+              </div>
+            </div>
+
             <br />
 
-            <button onClick={() => handleRoleSelect("admin")}>
+            {/* <button onClick={() => handleRoleSelect("admin")}>
               Administrador
             </button>
-            <button onClick={() => handleRoleSelect("player")}>Jugador</button>
+            <button onClick={() => handleRoleSelect("player")}>Jugador</button> */}
+
+            <div className="Buttons">
+                 <button className="btn" onClick={() => handleRoleSelect("admin")}>
+              <svg height={24} width={24} fill="#FFFFFF" viewBox="0 0 24 24" data-name="Layer 1" id="Layer_1"className="sparkle">
+                <path d="M10,21.236,6.755,14.745.264,11.5,6.755,8.255,10,1.764l3.245,6.491L19.736,11.5l-6.491,3.245ZM18,21l1.5,3L21,21l3-1.5L21,18l-1.5-3L18,18l-3,1.5ZM19.333,4.667,20.5,7l1.167-2.333L24,3.5,21.667,2.333,20.5,0,19.333,2.333,17,3.5Z" />
+              </svg>
+              <span className="text"> Administrador</span>
+            </button>
+
+             <button className="btn" onClick={() => handleRoleSelect("admin")}>
+              <svg height={24} width={24} fill="#FFFFFF" viewBox="0 0 24 24" data-name="Layer 1" id="Layer_1"className="sparkle">
+                <path d="M10,21.236,6.755,14.745.264,11.5,6.755,8.255,10,1.764l3.245,6.491L19.736,11.5l-6.491,3.245ZM18,21l1.5,3L21,21l3-1.5L21,18l-1.5-3L18,18l-3,1.5ZM19.333,4.667,20.5,7l1.167-2.333L24,3.5,21.667,2.333,20.5,0,19.333,2.333,17,3.5Z" />
+              </svg>
+              <span className="text"> Jugador</span>
+            </button>
+            </div>
+         
+
+
           </div>
         </div>
       </div>
